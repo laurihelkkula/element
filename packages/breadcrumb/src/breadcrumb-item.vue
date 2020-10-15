b0f1c34a2ed7b4fad7dbd27e4e3df5f905d5c502
Lauri Helkkula <lauri.helkkula.if@outlook.com>
2020-10-15 21:22:19 +0000

    fix breadcrumb memory leak

--- packages/breadcrumb/src/breadcrumb-item.vue
+++ packages/breadcrumb/src/breadcrumb-item.vue
@@ -3,7 +3,8 @@
     <span
       :class="['el-breadcrumb__inner', to ? 'is-link' : '']"
       ref="link"
-      role="link">
+      role="link"
+      @click="handleClick">
       <slot></slot>
     </span>
     <i v-if="separatorClass" class="el-breadcrumb__separator" :class="separatorClass"></i>
@@ -26,16 +27,19 @@
 
     inject: ['elBreadcrumb'],
 
+    methods: {
+      handleClick() {
+        const { to, $router } = this;
+        if (!to || !$router) return;
+        this.replace ? $router.replace(to) : $router.push(to);
+      },
+    },
+
     mounted() {
       this.separator = this.elBreadcrumb.separator;
       this.separatorClass = this.elBreadcrumb.separatorClass;
       const link = this.$refs.link;
       link.setAttribute('role', 'link');
-      link.addEventListener('click', _ => {
-        const { to, $router } = this;
-        if (!to || !$router) return;
-        this.replace ? $router.replace(to) : $router.push(to);
-      });
     }
   };
 </script>
